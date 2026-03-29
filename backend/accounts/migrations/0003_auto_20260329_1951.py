3746d75077de446f65d5c68d9666f2c48e03138f
Man12na <manasachowdhary1@gmail.com>
2026-03-30 01:24:29 +0530

    Add safe superuser migration for Render

---
+++ backend/accounts/migrations/0003_auto_20260329_1951.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+def create_admin_user(apps, schema_editor):
+    User = apps.get_model("auth", "User")
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="YourStrongPassword123"
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),  # always point to the initial migration
+        ('auth', '0012_alter_user_first_name_max_length'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_admin_user),
+    ]
