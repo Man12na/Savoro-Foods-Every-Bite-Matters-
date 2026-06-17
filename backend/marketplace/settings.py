8fb3309355f6fe8614cc9ba2c4f18fe6bad65b3e
Man12na <86163381+Man12na@users.noreply.github.com>
2026-06-17 21:49:59 +0530

    Update settings.py

--- backend/marketplace/settings.py
+++ backend/marketplace/settings.py
@@ -187,6 +187,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media
 #MEDIA_URL = '/media/'
